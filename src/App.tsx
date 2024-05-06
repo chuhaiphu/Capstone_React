084886d9cfa5a1db695d4777b3d8a315118bcc27
Doan Vinh Phu <doanvinhphu@Doans-MacBook-Pro.local>
2024-05-06 16:22:41 +0700

    add react  config

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import './assets/css/animate.css'
+import './assets/css/bootstrap.min.css'
+import './assets/css/jquery.animatedheadline.css'
+import './assets/css/magnific-popup.css'
+import './assets/css/main.css'
+import './assets/css/nice-select.css'
+import './assets/css/odometer.css'
+import './assets/css/owl.carousel.min.css'
+import './assets/css/owl.theme.default.min.css'
+import Header from './pages/Home/_components/Header'
+import '@fortawesome/fontawesome-free/css/all.min.css'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
     </div>
   );
 }
